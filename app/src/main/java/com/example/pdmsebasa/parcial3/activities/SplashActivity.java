db231b8989812ee8fd74deb17789f3226771126a
emartinezs <00116316@uca.edu.sv>
2018-07-10 09:27:08 -0600

    Splash activity

---
+++ app/src/main/java/com/example/pdmsebasa/parcial3/activities/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.pdmsebasa.parcial3.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.pdmsebasa.parcial3.R;
+
+public class SplashActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(this::startActivity(), 3000);
+    }
+
+    private void startActivity(){
+        Intent intent;
+        if (getToken().equals("")){
+            intent = new Intent(this, LoginActivity.class);
+        }else{
+            intent = new Intent(this, MainActivity.class);
+        }
+        startActivity(intent);
+        finish();
+    }
+
+    private String getToken(){
+        SharedPreferences sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+        return sharedPreferences.getString(getString(R.string.key_token), "");
+    }
+}
