a9aa717b091a1b2d208247c46a3fc041f16cb0bd
emartinezs <00116316@uca.edu.sv>
2018-07-10 08:56:32 -0600

    Adapter generico

--- app/src/main/java/com/example/pdmsebasa/parcial3/adapters/Adapter.java
+++ app/src/main/java/com/example/pdmsebasa/parcial3/adapters/Adapter.java
@@ -1,4 +1,50 @@
 package com.example.pdmsebasa.parcial3.adapters;
 
-public class Adapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.pdmsebasa.parcial3.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
+
+    private List<String> list = new ArrayList<>();
+
+    public Adapter(List<String> list){
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.generic_cardview, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String stringItem = list.get(position);
+        holder.textView.setText(stringItem);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.generic_cardview_textView);
+        }
+    }
 }
