6f3a720b6414a6cfe127fad5a5badf383f97380f
Pedro Gómez <00057616@uca.edu.sv>
2018-07-10 10:00:02 -0600

    modifique login

--- app/src/main/java/com/example/pdmsebasa/parcial3/activities/LoginActivity.java
+++ app/src/main/java/com/example/pdmsebasa/parcial3/activities/LoginActivity.java
@@ -4,22 +4,31 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+
+import com.example.pdmsebasa.parcial3.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextInputEditText username, password;
+    AppCompatButton button;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
         bindViews();
     }
 
 
     private void bindViews(){
-        //username=findViewById();
-        //password=findViewById();
+        username=findViewById(R.id.user);
+        password=findViewById(R.id.pass);
+        button=findViewById(R.id.btn_login);
+        button.setOnClickListener(v->login());
     }
+
+    private void login(){}
 
 }
