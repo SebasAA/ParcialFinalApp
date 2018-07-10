fe5a7b5eff6c6e00c9ec03c6a4a6c8e74f0854b6
Pedro Gómez <00057616@uca.edu.sv>
2018-07-10 15:19:44 -0600

    se agrega metodo para obtener rol

--- app/src/main/java/com/example/pdmsebasa/parcial3/api/CuteCharmsAPI.java
+++ app/src/main/java/com/example/pdmsebasa/parcial3/api/CuteCharmsAPI.java
@@ -3,6 +3,9 @@ package com.example.pdmsebasa.parcial3.api;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface CuteCharmsAPI {
@@ -10,8 +13,13 @@ public interface CuteCharmsAPI {
     String END_POINT="https://parcial-final-api.herokuapp.com/";
 
     @FormUrlEncoded
-    @POST("/auth/login")
+    @POST("auth/login")
     Call<String> getToken(@Field("nickname") String nickname, @Field("password")String pass);
+
+    @GET("api/user/detail")
+    Call<String> getUserRol(@Header("Authorization") String auth);
+
+  //  @GET("/api/products")
 
 
 }
