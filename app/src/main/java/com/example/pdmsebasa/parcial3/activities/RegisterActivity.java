e0ae05de63e9d7265cc1ff0e65df43714fb5400b
Pedro Gómez <00057616@uca.edu.sv>
2018-07-10 11:57:22 -0600

    fixin merge

---
+++ app/src/main/java/com/example/pdmsebasa/parcial3/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.pdmsebasa.parcial3.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.pdmsebasa.parcial3.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
