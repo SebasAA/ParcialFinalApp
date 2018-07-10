701902c44aa004e9ded53c04332d538b7795eb98
Pedro Gómez <00057616@uca.edu.sv>
2018-07-10 08:38:06 -0600

    se agrego actividad login

---
+++ app/src/main/java/com/example/pdmsebasa/parcial3/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.pdmsebasa.parcial3.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText username, password;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
