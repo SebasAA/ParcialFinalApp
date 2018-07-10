5abac9b50d3a69883a1e07c3377d4162d9cb2dd7
emartinezs <00116316@uca.edu.sv>
2018-07-10 14:09:24 -0600

    Toolbar

--- app/src/main/java/com/example/pdmsebasa/parcial3/activities/MainActivity.java
+++ app/src/main/java/com/example/pdmsebasa/parcial3/activities/MainActivity.java
@@ -1,25 +1,56 @@
 package com.example.pdmsebasa.parcial3.activities;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.pdmsebasa.parcial3.R;
 import com.example.pdmsebasa.parcial3.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity{
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        setupToolbar();
+        setHomeFragment();
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = findViewById(R.id.appsbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+    }
+
+    private void setHomeFragment(){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.content_frame, new HomeFragment());
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return false;
     }
 }
