858fc6973689cfd802a57fed651a0cb9053d0fa9
allenwalker2015 <00053812@uca.edu.sv>
2018-07-10 20:45:12 -0600

    Añadido adapter para la vista de administrador de productos

--- app/src/main/java/com/example/pdmsebasa/parcial3/adapters/ProductoUserAdapter.java
+++ app/src/main/java/com/example/pdmsebasa/parcial3/adapters/ProductoUserAdapter.java
@@ -5,7 +5,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.pdmsebasa.parcial3.R;
@@ -15,18 +14,18 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductoAdapter extends RecyclerView.Adapter<ProductoAdapter.ViewHolder>{
+public class ProductoUserAdapter extends RecyclerView.Adapter<ProductoUserAdapter.ViewHolder> {
 
     private List<Producto> list = new ArrayList<>();
 
-    public ProductoAdapter(List<Producto> list){
+    public ProductoUserAdapter(List<Producto> list) {
         this.list = list;
     }
 
     /**
      * @param list Lista a mostrar en {@link RecyclerView}
      */
-    public void setList(List<Producto> list){
+    public void setList(List<Producto> list) {
         this.list = list;
     }
 
@@ -42,34 +41,29 @@ public class ProductoAdapter extends RecyclerView.Adapter<ProductoAdapter.ViewHo
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Producto ProductoItem = list.get(position);
         String price_and_gain;
-        Float gain = ProductoItem.getPrice()- ProductoItem.getCosto();
-        price_and_gain = "$"+ ProductoItem.getPrice() + " - " + truncFloat(gain);
+        Float gain = ProductoItem.getPrice() - ProductoItem.getCosto();
+        price_and_gain = "$" + ProductoItem.getPrice() + " - " + truncFloat(gain);
         holder.name.setText(ProductoItem.getName());
         holder.price.setText(price_and_gain);
     }
 
     @Override
     public int getItemCount() {
-        return list==null?0:list.size();
+        return list == null ? 0 : list.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        ImageView less, more;
-        private TextView name, price, quantity;
+    public String truncFloat(Float number) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        return df.format(number);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView name, price;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            name = itemView.findViewById(R.id.venta_precio);
-            price = itemView.findViewById(R.id.venta_nombre_cliente2);//change id later
-            quantity = itemView.findViewById(R.id.textView2);
-            less = itemView.findViewById(R.id.imageView2);
-            more = itemView.findViewById(R.id.imageView3);
-
+            name = itemView.findViewById(R.id.nombreProducto);
+            price = itemView.findViewById(R.id.precioProducto);//change id later
         }
-    }
-
-    public String truncFloat(Float number){
-        DecimalFormat df = new DecimalFormat("#.00");
-        return df.format(number);
     }
 }
