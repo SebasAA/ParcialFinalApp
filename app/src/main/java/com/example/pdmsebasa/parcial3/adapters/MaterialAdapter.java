b4591bee26b6a8efe7eb17f8b9e812a46ce82c0d
allenwalker2015 <00053812@uca.edu.sv>
2018-07-10 20:26:20 -0600

    Merge con UI

---
+++ app/src/main/java/com/example/pdmsebasa/parcial3/adapters/MaterialAdapter.java
@@ -0,0 +1,71 @@
+package com.example.pdmsebasa.parcial3.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.pdmsebasa.parcial3.R;
+import com.example.pdmsebasa.parcial3.database.entities.Producto;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaterialAdapter extends RecyclerView.Adapter<MaterialAdapter.ViewHolder> {
+
+    private List<Producto> list = new ArrayList<>();
+
+    public MaterialAdapter(List<Producto> list) {
+        this.list = list;
+    }
+
+    /**
+     * @param list Lista a mostrar en {@link RecyclerView}
+     */
+    public void setList(List<Producto> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.generic_cardview, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Producto ProductoItem = list.get(position);
+        String price_and_gain;
+        Float gain = ProductoItem.getPrice() - ProductoItem.getCosto();
+        price_and_gain = "$" + ProductoItem.getPrice() + " - " + truncFloat(gain);
+        holder.name.setText(ProductoItem.getName());
+        holder.price.setText(price_and_gain);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list == null ? 0 : list.size();
+    }
+
+    public String truncFloat(Float number) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        return df.format(number);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView name, price;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.generic_cardview_textView);
+            price = itemView.findViewById(R.id.generic_cardview_textView);//change id later
+
+
+        }
+    }
+}
